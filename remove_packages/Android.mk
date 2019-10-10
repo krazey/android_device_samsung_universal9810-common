6f47b0a5e880531266afd7bddf1bcf4f3ed04247
synt4x93 <synt4x.93@gmail.com>
2019-10-10 20:15:51 +0000

    universal9810: remove_packages: phototable

--- remove_packages/Android.mk
+++ remove_packages/Android.mk
@@ -5,7 +5,7 @@ LOCAL_MODULE := RemovePackages
 LOCAL_SRC_FILES := false
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := APPS
-LOCAL_OVERRIDES_PACKAGES := AudioFX Updater Email Exchange Exchange2 UnifiedEmail
+LOCAL_OVERRIDES_PACKAGES := AudioFX Updater Email Exchange Exchange2 UnifiedEmail PhotoTable
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
 include $(BUILD_PREBUILT)
