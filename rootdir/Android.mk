39bcb3dcfcdb1f5e9f4c21def63737556b6bf8fb
newrocklsm <lewis@abtekk.co.uk>
2019-12-13 09:50:38 +0000

    universal: ramdisk: drop ramdisk files in to /vendor

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -4,40 +4,41 @@ include $(CLEAR_VARS)
 LOCAL_MODULE            := fstab.samsungexynos9810
 LOCAL_MODULE_CLASS      := BOOT
 LOCAL_SRC_FILES         := etc/fstab.samsungexynos9810
-LOCAL_MODULE_PATH       := $(TARGET_ROOT_OUT)
+LOCAL_MODULE_PATH       := $(TARGET_OUT_VENDOR_ETC)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE            := init.samsung.rc
 LOCAL_MODULE_CLASS      := BOOT
 LOCAL_SRC_FILES         := etc/init.samsung.rc
-LOCAL_MODULE_PATH       := $(TARGET_ROOT_OUT)
+LOCAL_MODULE_PATH       := $(TARGET_OUT_VENDOR_ETC)/init/hw
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE            := init.samsungexynos9810.rc
 LOCAL_MODULE_CLASS      := BOOT
 LOCAL_SRC_FILES         := etc/init.samsungexynos9810.rc
-LOCAL_MODULE_PATH       := $(TARGET_ROOT_OUT)
+LOCAL_MODULE_PATH       := $(TARGET_OUT_VENDOR_ETC)/init/hw
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE            := init.samsungexynos9810.usb.rc
 LOCAL_MODULE_CLASS      := BOOT
 LOCAL_SRC_FILES         := etc/init.samsungexynos9810.usb.rc
-LOCAL_MODULE_PATH       := $(TARGET_ROOT_OUT)
+LOCAL_MODULE_PATH       := $(TARGET_OUT_VENDOR_ETC)/init/hw
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE            := init.baseband.rc
 LOCAL_MODULE_CLASS      := BOOT
 LOCAL_SRC_FILES         := etc/init.baseband.rc
-LOCAL_MODULE_PATH       := $(TARGET_ROOT_OUT)
+LOCAL_MODULE_PATH       := $(TARGET_OUT_VENDOR_ETC)/init/hw
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE            := ueventd.samsungexynos9810.rc
+LOCAL_MODULE_STEM       := ueventd.rc
 LOCAL_MODULE_CLASS      := BOOT
 LOCAL_SRC_FILES         := etc/ueventd.samsungexynos9810.rc
-LOCAL_MODULE_PATH       := $(TARGET_ROOT_OUT)
+LOCAL_MODULE_PATH       := $(TARGET_OUT_VENDOR)
 include $(BUILD_PREBUILT)
