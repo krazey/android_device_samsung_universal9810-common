90b04ea47e77026de6ee6bdb1a6c2f11c641d1dc
newrocklsm <lewis@abtekk.co.uk>
2019-12-13 10:13:02 +0000

    universal: build sensors implementation

--- universal9810-common.mk
+++ universal9810-common.mk
@@ -251,7 +251,12 @@ PRODUCT_PACKAGES += \
 
 # Lights
 PRODUCT_PACKAGES += \
+    android.hardware.light@2.0-impl \
     android.hardware.light@2.0-service.9810
+
+# Sensors
+PRODUCT_PACKAGES += \
+    android.hardware.sensors@1.0-impl
 
 # Fingerprint
 PRODUCT_PACKAGES += \
