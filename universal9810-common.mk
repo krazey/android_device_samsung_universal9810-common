d0712349699a5c6dcaeb6d88428108195586a67f
Lewis Smith <lewis@abtekk.co.uk>
2019-12-17 20:55:34 +0000

    universal: build sensors service/HAL

--- universal9810-common.mk
+++ universal9810-common.mk
@@ -284,7 +284,9 @@ PRODUCT_PACKAGES += \
 
 # Sensors
 PRODUCT_PACKAGES += \
-    android.hardware.sensors@1.0-impl
+	android.hardware.sensors@1.0 \
+	android.hardware.sensors@1.0-impl \
+    android.hardware.sensors@1.0-service
 
 # Fingerprint
 PRODUCT_PACKAGES += \
