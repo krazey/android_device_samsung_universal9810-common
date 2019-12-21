7b09ab484cc921fd79b9ec93114252caa57f3506
synt4x93 <synt4x.93@gmail.com>
2019-12-21 21:14:36 +0000

    universal9810: releasetools: run resize2fs on /vendor

--- releasetools/releasetools.py
+++ releasetools/releasetools.py
@@ -19,3 +19,6 @@ def FullOTA_PostValidate(info):
     info.script.AppendExtra('run_program("/sbin/e2fsck", "-fy", "/dev/block/platform/11120000.ufs/by-name/SYSTEM");');
     info.script.AppendExtra('run_program("/sbin/resize2fs", "/dev/block/platform/11120000.ufs/by-name/SYSTEM");');
     info.script.AppendExtra('run_program("/sbin/e2fsck", "-fy", "/dev/block/platform/11120000.ufs/by-name/SYSTEM");');
+    info.script.AppendExtra('run_program("/sbin/e2fsck", "-fy", "/dev/block/platform/11120000.ufs/by-name/VENDOR");');
+    info.script.AppendExtra('run_program("/sbin/resize2fs", "/dev/block/platform/11120000.ufs/by-name/VENDOR");');
+    info.script.AppendExtra('run_program("/sbin/e2fsck", "-fy", "/dev/block/platform/11120000.ufs/by-name/VENDOR");');
