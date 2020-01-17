d2d16a32418422137d4d96177f2ac60d47c20daa
Lewis Smith <lewis@abtekk.co.uk>
2020-01-17 15:18:13 +0000

    universal: Build new light HAL from source

--- universal9810-common.mk
+++ universal9810-common.mk
@@ -275,7 +275,7 @@ PRODUCT_PACKAGES += \
 # Lights
 PRODUCT_PACKAGES += \
     android.hardware.light@2.0-impl \
-    android.hardware.light@2.0-service.9810
+    android.hardware.light@2.0-service.samsung
 
 # Sensors
 PRODUCT_PACKAGES += \
