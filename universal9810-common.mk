e44b033e7996e61073c18580e4020697635f3142
Lewis Smith <lewis@abtekk.co.uk>
2020-01-23 10:45:39 +0000

    universal: move vendor call to device repo

--- universal9810-common.mk
+++ universal9810-common.mk
@@ -282,5 +282,3 @@ PRODUCT_PACKAGES += \
 -include $(LOCAL_PATH)/system_prop.mk
 
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
-# Call proprietary blob setup
-$(call inherit-product, vendor/samsung/universal9810-common/universal9810-common-vendor.mk)
